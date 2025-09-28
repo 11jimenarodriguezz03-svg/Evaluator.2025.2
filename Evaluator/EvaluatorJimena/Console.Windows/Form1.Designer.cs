630dfbc7d13e54f5ef83758ad0bd5232084db53c
MELLIZA JIMENA PATERNINA RODRGUEZ <11jimena.rodriguezz03@gmail.com>
2025-09-28 18:19:39 -0500

    Basic proyec

--- Evaluator/EvaluatorJimena/Console.Windows/Form1.Designer.cs
+++ Evaluator/EvaluatorJimena/Console.Windows/Form1.Designer.cs
@@ -154,32 +154,35 @@
             // 
             // BtnOpen
             // 
+            BtnOpen.BackColor = Color.FromArgb(255, 128, 0);
             BtnOpen.Location = new Point(395, 170);
             BtnOpen.Name = "BtnOpen";
             BtnOpen.Size = new Size(94, 57);
             BtnOpen.TabIndex = 10;
             BtnOpen.Text = "(";
-            BtnOpen.UseVisualStyleBackColor = true;
+            BtnOpen.UseVisualStyleBackColor = false;
             BtnOpen.Click += BtnOpen_Click;
             // 
             // BtnMul
             // 
+            BtnMul.BackColor = Color.FromArgb(255, 128, 0);
             BtnMul.Location = new Point(395, 233);
             BtnMul.Name = "BtnMul";
             BtnMul.Size = new Size(94, 59);
             BtnMul.TabIndex = 11;
             BtnMul.Text = "*";
-            BtnMul.UseVisualStyleBackColor = true;
+            BtnMul.UseVisualStyleBackColor = false;
             BtnMul.Click += BtnMul_Click;
             // 
             // BtnPlus
             // 
+            BtnPlus.BackColor = Color.FromArgb(255, 128, 0);
             BtnPlus.Location = new Point(395, 298);
             BtnPlus.Name = "BtnPlus";
             BtnPlus.Size = new Size(94, 59);
             BtnPlus.TabIndex = 12;
             BtnPlus.Text = "+";
-            BtnPlus.UseVisualStyleBackColor = true;
+            BtnPlus.UseVisualStyleBackColor = false;
             BtnPlus.Click += BtnPlus_Click;
             // 
             // BtnDot
@@ -194,86 +197,95 @@
             // 
             // BtnEqual
             // 
+            BtnEqual.BackColor = Color.FromArgb(255, 128, 0);
             BtnEqual.Location = new Point(395, 365);
             BtnEqual.Name = "BtnEqual";
             BtnEqual.Size = new Size(294, 70);
             BtnEqual.TabIndex = 14;
             BtnEqual.Text = "=";
-            BtnEqual.UseVisualStyleBackColor = true;
+            BtnEqual.UseVisualStyleBackColor = false;
             BtnEqual.Click += BtnEqual_Click;
             // 
             // BtnClose
             // 
+            BtnClose.BackColor = Color.FromArgb(255, 128, 0);
             BtnClose.Location = new Point(495, 170);
             BtnClose.Name = "BtnClose";
             BtnClose.Size = new Size(94, 57);
             BtnClose.TabIndex = 15;
             BtnClose.Text = ")";
-            BtnClose.UseVisualStyleBackColor = true;
+            BtnClose.UseVisualStyleBackColor = false;
             BtnClose.Click += BtnClose_Click;
             // 
             // BntDiv
             // 
+            BntDiv.BackColor = Color.FromArgb(255, 128, 0);
             BntDiv.Location = new Point(495, 233);
             BntDiv.Name = "BntDiv";
             BntDiv.Size = new Size(94, 59);
             BntDiv.TabIndex = 16;
             BntDiv.Text = "/";
-            BntDiv.UseVisualStyleBackColor = true;
+            BntDiv.UseVisualStyleBackColor = false;
             BntDiv.Click += BntDiv_Click;
             // 
             // BtnMin
             // 
+            BtnMin.BackColor = Color.FromArgb(255, 128, 0);
             BtnMin.Location = new Point(495, 297);
             BtnMin.Name = "BtnMin";
             BtnMin.Size = new Size(94, 60);
             BtnMin.TabIndex = 17;
             BtnMin.Text = "-";
-            BtnMin.UseVisualStyleBackColor = true;
+            BtnMin.UseVisualStyleBackColor = false;
             BtnMin.Click += BtnMin_Click;
             // 
             // BtnPow
             // 
+            BtnPow.BackColor = Color.FromArgb(255, 128, 0);
             BtnPow.Location = new Point(595, 295);
             BtnPow.Name = "BtnPow";
             BtnPow.Size = new Size(94, 65);
             BtnPow.TabIndex = 18;
             BtnPow.Text = "^";
-            BtnPow.UseVisualStyleBackColor = true;
+            BtnPow.UseVisualStyleBackColor = false;
             BtnPow.Click += BtnPow_Click;
             // 
             // BtnClear
             // 
+            BtnClear.BackColor = Color.FromArgb(255, 128, 0);
             BtnClear.Location = new Point(595, 233);
             BtnClear.Name = "BtnClear";
             BtnClear.Size = new Size(94, 56);
             BtnClear.TabIndex = 19;
             BtnClear.Text = "Clear";
-            BtnClear.UseVisualStyleBackColor = true;
+            BtnClear.UseVisualStyleBackColor = false;
             BtnClear.Click += BtnClear_Click;
             // 
             // BtnDelete
             // 
+            BtnDelete.BackColor = Color.FromArgb(255, 128, 0);
             BtnDelete.Location = new Point(595, 170);
             BtnDelete.Name = "BtnDelete";
             BtnDelete.Size = new Size(94, 57);
             BtnDelete.TabIndex = 20;
             BtnDelete.Text = "Delete";
-            BtnDelete.UseVisualStyleBackColor = true;
+            BtnDelete.UseVisualStyleBackColor = false;
             BtnDelete.Click += BtnDelete_Click;
             // 
             // TexDisplay
             // 
-            TexDisplay.Location = new Point(102, 93);
+            TexDisplay.BackColor = Color.Lime;
+            TexDisplay.Location = new Point(96, 93);
             TexDisplay.Multiline = true;
             TexDisplay.Name = "TexDisplay";
-            TexDisplay.Size = new Size(587, 61);
+            TexDisplay.Size = new Size(593, 61);
             TexDisplay.TabIndex = 21;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
             ClientSize = new Size(953, 625);
             Controls.Add(TexDisplay);
             Controls.Add(BtnDelete);
